403a58090cd63ed01c4011b8db45e15e6ce0966d
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-02 00:20:02 -0500

    callback interface doesn't belong here IMO, but because we can't do circular imports, it has to go here. add public methods to manipulate event callbacks

--- xgbutil.go
+++ xgbutil.go
@@ -20,7 +20,27 @@ type XUtil struct {
     root xgb.Id
     atoms map[string]xgb.Id
     atomNames map[xgb.Id]string
+    callbacks map[int]map[xgb.Id][]Callback // ev code -> win -> callbacks
 }
+
+// Callback is an interface that should be implemented by event callback 
+// functions. Namely, to assign a function to a particular event/window
+// combination, simply define a function with type '|Event|Fun' (pre-defined
+// in xevent/callback.go), and call the 'Connect' method.
+// The 'Run' method is used inside the Main event loop, and shouldn't be used
+// by the user.
+// Also, it is perfectly legitimate to connect to events that don't specify
+// a window (like MappingNotify and KeymapNotify). In this case, simply
+// use 'xgbutil.NoWindow' as the window id.
+type Callback interface {
+    Connect(xu *XUtil, evtype int, win xgb.Id)
+    Run(xu *XUtil, ev interface{})
+}
+
+// Sometimes we need to specify NO WINDOW when a window is typically
+// expected. (Like connecting to MappingNotify or KeymapNotify events.)
+// Use this value to do that.
+var NoWindow xgb.Id = 0
 
 type XError struct {
     funcName string // some identifier so we know where the error comes from
@@ -70,6 +90,7 @@ func Dial(display string) (*XUtil, error) {
         root: c.DefaultScreen().Root,
         atoms: make(map[string]xgb.Id, 50), // start with a nice size
         atomNames: make(map[xgb.Id]string, 50),
+        callbacks: make(map[int]map[xgb.Id][]Callback, 33),
     }
 
     // Register the Xinerama extension... because it doesn't cost much.
@@ -104,6 +125,33 @@ func (xu *XUtil) SetRootWin(root xgb.Id) {
     xu.root = root
 }
 
+// AttachCallback associates a (event, window) tuple with an event.
+func (xu *XUtil) AttachCallback(evtype int, win xgb.Id, fun Callback) {
+    // Do we need to allocate?
+    if _, ok := xu.callbacks[evtype]; !ok {
+        xu.callbacks[evtype] = make(map[xgb.Id][]Callback, 10)
+    }
+    if _, ok := xu.callbacks[evtype][win]; !ok {
+        xu.callbacks[evtype][win] = make([]Callback, 0)
+    }
+    xu.callbacks[evtype][win] = append(xu.callbacks[evtype][win], fun)
+}
+
+// RunCallbacks executes every callback corresponding to a
+// particular event/window tuple.
+func (xu *XUtil) RunCallbacks(event interface{}, evtype int, win xgb.Id) {
+    for _, cb := range xu.callbacks[evtype][win] {
+        cb.Run(xu, event)
+    }
+}
+
+// DetachWindow removes all callbacks associated with a particular window.
+func (xu *XUtil) DetachWindow(win xgb.Id) {
+    for evtype, _ := range xu.callbacks {
+        delete(xu.callbacks[evtype], win)
+    }
+}
+
 // GetAtom retrieves an atom identifier from a cache if it exists.
 func (xu *XUtil) GetAtom(name string) (aid xgb.Id, ok bool) {
     aid, ok = xu.atoms[name]
@@ -121,6 +169,9 @@ func (xu *XUtil) CacheAtom(name string, aid xgb.Id) {
     xu.atoms[name] = aid
     xu.atomNames[aid] = name
 }
+
+
+// True utility/misc functions. Could be factored out at some point.
 
 // BeSafe will recover from any panic produced by xgb or xgbutil and transform
 // it into an idiomatic Go error as a second return value.
