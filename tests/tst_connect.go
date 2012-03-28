50ea58806b2daba52905f2c2d9ba4a9c63b26617
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-28 06:42:20 -0400

    a simple program to test connection with X

---
+++ tests/tst_connect.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+)
+
+import (
+    "github.com/BurntSushi/xgbutil"
+)
+
+func main() {
+    _, err := xgbutil.Dial("")
+    if err != nil {
+        log.Fatalf("Could not connect to X: %v", err)
+    }
+
+    fmt.Println("Connected!")
+}
+
