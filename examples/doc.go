e8602e5def1c4b637f64e408c9e632f726fb8abf
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-05-14 01:15:21 -0400

    a bit more about the examples

--- examples/doc.go
+++ examples/doc.go
@@ -1,5 +1,20 @@
 /*
 Package examples contains several complete examples depicting some common use
 cases of xgbutil.
+
+The examples included are designed to either demonstrate how a particular
+facility of xgbutil works, or how to accomplish some common goal when using X.
+
+For example, example simple-keybinding demonstrates how to use xgbutil to
+respond to particular key presses while example graceful-window-close shows
+how to close windows without killing your connection to X.
+
+My goal is that each example demonstrates one thing, although the nature of X
+typically requires some amount of boilerplate so that some examples are a bit
+more bloated than I would like.
+
+If you have any suggestions for other examples to include, I'd be happy to hear
+them. You may post them at the main project page:
+https://github.com/BurntSushi/xgbutil.
 */
 package documentation
