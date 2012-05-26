ac950939f19ca7c34a552ca7ff140a4424d07d2b
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-05-26 00:30:51 -0400

    Show how to take a screenshot using the xgraphics package.

---
+++ examples/screenshot/main.go
@@ -0,0 +1,43 @@
+// Example screenshot shows how to take a screenshot of the current desktop
+// and show it in a window. In a comment, it also shows how to save it as
+// a png.
+//
+// It works by getting the image of the root window, which automatically
+// includes all child windows.
+package main
+
+import (
+	"log"
+
+	"github.com/BurntSushi/xgb/xproto"
+
+	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/xevent"
+	"github.com/BurntSushi/xgbutil/xgraphics"
+)
+
+func main() {
+	X, err := xgbutil.NewConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Use the "NewDrawable" constructor to create an xgraphics.Image value
+	// from a drawable. (Usually this is done with pixmaps, but drawables
+	// can also be windows.)
+	ximg, err := xgraphics.NewDrawable(X, xproto.Drawable(X.RootWin()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Shows the screenshot in a window.
+	ximg.XShowExtra("Screenshot", true)
+
+	// If you'd like to save it as a png, use:
+	// err = ximg.SavePng("screenshot.png") 
+	// if err != nil { 
+	// log.Fatal(err) 
+	// } 
+
+	xevent.Main(X)
+}
