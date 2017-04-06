a5eeb4e3dced7431afe8933f2f29878fa0d0eb3b
Dominik Honnef <dominik@honnef.co>
2017-04-06 06:02:39 +0200

    Respect Mode_switch and AltGr in keybind.LookupString

--- keybind/encoding.go
+++ keybind/encoding.go
@@ -27,31 +27,55 @@ import (
 // are mapped to particular modifiers (i.e., "XK_Caps_Lock" to "Lock" modifier).
 // We just check if the modifiers are activated. That's good enough for me.
 // XXX: We ignore num lock stuff.
-// XXX: We ignore MODE SWITCH stuff. (i.e., we don't use group 2 key syms.)
 func LookupString(xu *xgbutil.XUtil, mods uint16,
 	keycode xproto.Keycode) string {
 
-	k1, k2, _, _ := interpretSymList(xu, keycode)
+	modMap := ModMapGet(xu)
+	symToMod := map[xproto.Keysym]uint16{}
+	for i, kc := range modMap.Keycodes {
+		if kc == 0 {
+			continue
+		}
+		// map keysyms to modifiers. we're only really interested in
+		// Mode_switch, ISO_Level3_Shift and Num_Lock, though.
+		symToMod[KeysymGet(xu, kc, 0)] = Modifiers[byte(i)/modMap.KeycodesPerModifier]
+	}
+
+	k1, k2, k3, k4, k5, k6 := interpretSymList(xu, keycode)
 
 	shift := mods&xproto.ModMaskShift > 0
 	lock := mods&xproto.ModMaskLock > 0
+	mode := mods&symToMod[keysyms["Mode_switch"]] > 0
+	level3 := mods&symToMod[keysyms["ISO_Level3_Shift"]] > 0
+
+	var group []string
+	switch {
+	case level3:
+		group = []string{k5, k6}
+	case mode:
+		group = []string{k3, k4}
+	default:
+		group = []string{k1, k2}
+	}
+	// TODO(dh): do we need to handle ISO_Level3_Lock as well, or is
+	// the X server doing that for us?
 	switch {
 	case !shift && !lock:
-		return k1
+		return group[0]
 	case !shift && lock:
-		if len(k1) == 1 && unicode.IsLower(rune(k1[0])) {
-			return k2
+		if len(group[0]) == 1 && unicode.IsLower(rune(group[0][0])) {
+			return group[1]
 		} else {
-			return k1
+			return group[0]
 		}
 	case shift && lock:
-		if len(k2) == 1 && unicode.IsLower(rune(k2[0])) {
-			return string(unicode.ToUpper(rune(k2[0])))
+		if len(group[1]) == 1 && unicode.IsLower(rune(group[1][0])) {
+			return string(unicode.ToUpper(rune(group[1][0])))
 		} else {
-			return k2
+			return group[1]
 		}
 	case shift:
-		return k2
+		return group[1]
 	}
 
 	return ""
@@ -82,29 +106,45 @@ func KeyMatch(xu *xgbutil.XUtil,
 // interpretSymList interprets the keysym list for a particular keycode as
 // described in the third and fourth paragraphs of http://goo.gl/qum9q
 func interpretSymList(xu *xgbutil.XUtil, keycode xproto.Keycode) (
-	k1 string, k2 string, k3 string, k4 string) {
+	k1, k2, k3, k4, k5, k6 string) {
 
 	ks1 := KeysymGet(xu, keycode, 0)
 	ks2 := KeysymGet(xu, keycode, 1)
 	ks3 := KeysymGet(xu, keycode, 2)
 	ks4 := KeysymGet(xu, keycode, 3)
+	ks5 := KeysymGet(xu, keycode, 4)
+	ks6 := KeysymGet(xu, keycode, 5)
 
 	// follow the rules, third paragraph
 	switch {
 	case ks2 == 0 && ks3 == 0 && ks4 == 0:
+		// If the list (ignoring trailing NoSymbol entries) is a
+		// single KeySym ``K'', then the list is treated as if it were
+		// the list ``K NoSymbol K NoSymbol''.
 		ks3 = ks1
 	case ks3 == 0 && ks4 == 0:
+		// If the list (ignoring trailing NoSymbol entries) is a pair
+		// of KeySyms ``K1 K2'', then the list is treated as if it
+		// were the list ``K1 K2 K1 K2''.
 		ks3 = ks1
 		ks4 = ks2
 	case ks4 == 0:
+		// If the list (ignoring trailing NoSymbol entries) is a
+		// triple of KeySyms ``K1 K2 K3'', then the list is treated as
+		// if it were the list ``K1 K2 K3 NoSymbol''.
 		ks4 = 0
 	}
+
+	// TODO(dh): The document doesn't specify any rules for keysyms 5
+	// and 6. For now, we'll do nothing.
 
 	// Now convert keysyms to strings, so we can do alphabetic shit.
 	k1 = KeysymToStr(ks1)
 	k2 = KeysymToStr(ks2)
 	k3 = KeysymToStr(ks3)
 	k4 = KeysymToStr(ks4)
+	k5 = KeysymToStr(ks5)
+	k6 = KeysymToStr(ks6)
 
 	// follow the rules, fourth paragraph
 	if k2 == "" {
@@ -123,6 +163,9 @@ func interpretSymList(xu *xgbutil.XUtil, keycode xproto.Keycode) (
 			k4 = k3
 		}
 	}
+
+	// TODO(dh): Again, no rules are specified for groups 3 and 4, so
+	// we'll not do anything for now.
 
 	return
 }
